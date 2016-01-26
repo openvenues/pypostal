533ca889e29f90d27a7eda1851bdeb383ea6abf0
Al <albarrentine@gmail.com>
2016-01-26 02:13:53 -0500

    [build] setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+import subprocess
+import sys
+
+from setuptools import setup, Extension, Command, find_packages
+from setuptools.command.build_py import build_py
+from setuptools.command.build_ext import build_ext
+from setuptools.command.install import install
+from distutils.errors import DistutilsArgError
+
+this_dir = os.path.realpath(os.path.dirname(__file__))
+
+
+def main():
+    setup(
+        name='pypostal',
+        version='0.2',
+        install_requires=[
+            'six',
+        ],
+        ext_modules=[
+            Extension('postal._expand',
+                      sources=['postal/pyexpand.c'],
+                      libraries=['postal'],
+                      extra_compile_args=['-std=c99',
+                                          '-Wno-unused-function'],
+                      ),
+            Extension('postal._parser',
+                      sources=['postal/pyparser.c'],
+                      libraries=['postal'],
+                      extra_compile_args=['-std=c99',
+                                          '-Wno-unused-function'],
+                      ),
+        ],
+        packages=find_packages(),
+        zip_safe=False,
+        url='http://mapzen.com',
+        description='Fast address standardization and deduplication',
+        license='MIT License',
+        maintainer='mapzen.com',
+        maintainer_email='pelias@mapzen.com'
+    )
+
+
+if __name__ == '__main__':
+    main()
