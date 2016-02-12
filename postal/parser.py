a6c22f03ab4ad4c02850e03d9cb15158cb0156b6
Al <albarrentine@gmail.com>
2016-02-12 18:36:22 -0500

    [fix] PEP8

--- postal/parser.py
+++ postal/parser.py
@@ -1,14 +1,15 @@
-import _parser
+"""Python bindings to libpostal parse_address."""
+from postal import _parser
 from postal.utils.encoding import safe_decode
-
-DEFAULT_LANGUAGES = ('en',)
 
 
 def parse_address(address, language=None, country=None):
-    '''
+    """
+    Parse address into components.
+
     @param address: the address as either Unicode or a UTF-8 encoded string
     @param language (optional): language code
     @param country (optional): country code
-    '''
+    """
     address = safe_decode(address, 'utf-8')
     return _parser.parse_address(address, language=language, country=country)
