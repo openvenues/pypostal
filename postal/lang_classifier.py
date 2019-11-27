849622747a22bac142200aa528ff5aed76b7acac
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2019-11-27 15:42:10 +0100

    Check for exception in cases where address=None

--- postal/lang_classifier.py
+++ postal/lang_classifier.py
@@ -2,7 +2,6 @@
 from postal import _langclassifier
 from postal.utils.encoding import safe_decode
 
-
 def classify_lang_address(address):
     """
     Classify the language of an address.
@@ -10,4 +9,7 @@ def classify_lang_address(address):
     @param address: the address as either Unicode or a UTF-8 encoded string
     """
     address = safe_decode(address, 'utf-8')
-    return _langclassifier.classify_lang_address(address)
+    try:
+        return _langclassifier.classify_lang_address(address)
+    except SystemError:
+        return None
