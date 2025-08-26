822f91b83260fc87291e90616d347296642f14e4
Daniel <DannyTheHeretic@gmail.com>
2025-08-25 20:13:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -87,6 +87,23 @@ def main():
         license='MIT License',
         maintainer='mapzen.com',
         maintainer_email='pelias@mapzen.com',
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Intended Audience :: Information Technology',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: C',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX :: Linux',
+            'Topic :: Text Processing :: Linguistic',
+            'Topic :: Scientific/Engineering :: GIS',
+            'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
     )
 
 
