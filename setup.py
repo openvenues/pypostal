625d96bc0497f7c21ce96209b50c834feb6bbd1a
Al <albarrentine@gmail.com>
2025-10-28 23:37:05 -0400

    [fix] simplifying setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ this_dir = os.path.realpath(os.path.dirname(__file__))
 VERSION = '1.1.10'
 
 
-def _check_run(*cmd_parts):
+def pkgconf_output(*options):
     return subprocess.run(
-        cmd_parts,
+        ['pkgconf', '--print-errors'] + list(options) + ['libpostal'],
         stdin=subprocess.DEVNULL,
         check=True,
         capture_output=True,
@@ -20,63 +20,21 @@ def _check_run(*cmd_parts):
     ).stdout
 
 
-def _pkgconf_getflags(flag):
-    return _check_run('pkgconf', '--print-errors', flag, 'libpostal')
+include_dirs = ['/usr/local/include']
+library_dirs = ['/usr/local/lib']
 
 
-def _pkgconf_parsed(flag):
-    res = _pkgconf_getflags(flag)
-    return [part[2:] for part in res.strip().split(' ')]
-
-
-def libpostal_flags_from_env():
-    prefix = os.getenv('LIBPOSTAL_PREFIX')
-    if prefix is None:
-        return None
-
-    return {
-        'include_dirs': [os.path.join(prefix, 'include')],
-        'library_dirs': [os.path.join(prefix, 'libs')],
-    }
-
-
-def libpostal_flags_from_pkgconf():
+prefix = os.getenv('LIBPOSTAL_PREFIX')
+if prefix is not None:
+    include_dirs = [os.path.join(prefix, 'include')]
+    library_dirs = [os.path.join(prefix, 'lib')]
+else:
     try:
-        _pkgconf_getflags('--exists')
-        return {
-            'include_dirs': _pkgconf_parsed('--cflags-only-I'),
-            'library_dirs': _pkgconf_parsed('--libs-only-L'),
-        }
-    except FileNotFoundError:
-        # If we get this exception, pkgconf was not found
-        return None
-    except CalledProcessError as e:
-        if e.returncode == 1:
-            # No libpostal.pc found
-            return None
-
-        raise
-
-
-def libpostal_flags():
-    # First, check the env
-    config = libpostal_flags_from_env()
-    if config is not None:
-        return config
-
-    # Then see if pkgconf can find it
-    config = libpostal_flags_from_pkgconf()
-    if config is not None:
-        return config
-
-    # If neither of those worked, just guess the default
-    return {
-        'include_dirs': ['/usr/local/include'],
-        'library_dirs': ['/usr/local/libs'],
-    }
-
-
-_libpostal_flags = libpostal_flags()
+        pkgconf_output('--exists')
+        include_dirs = [part[2:] for part in pkgconf_output('--cflags-only-I').strip().split()]
+        library_dirs = [part[2:] for part in pkgconf_output('--libs-only-L').strip().split()]
+    except (FileNotFoundError, CalledProcessError):
+        pass
 
 
 def main():
@@ -93,50 +51,50 @@ def main():
             Extension('postal._expand',
                       sources=['postal/pyexpand.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._parser',
                       sources=['postal/pyparser.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._token_types',
                       sources=['postal/pytokentypes.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._tokenize',
                       sources=['postal/pytokenize.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._normalize',
                       sources=['postal/pynormalize.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._near_dupe',
                       sources=['postal/pyneardupe.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
             Extension('postal._dedupe',
                       sources=['postal/pydedupe.c', 'postal/pyutils.c'],
                       libraries=['postal'],
-                      include_dirs=_libpostal_flags['include_dirs'],
-                      library_dirs=_libpostal_flags['library_dirs'],
+                      include_dirs=include_dirs,
+                      library_dirs=library_dirs,
                       extra_compile_args=['-std=c99'],
                       ),
         ],
